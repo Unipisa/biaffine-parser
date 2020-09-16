620f84eb9b82d389d0bbc86ba4eadbe744becaab
Daniele Sartiano <daniele.sartiano@gmail.com>
2020-09-16 16:35:01 +0200

    Updated tokenizer tests

--- tests/test_tokenizer.py
+++ tests/test_tokenizer.py
@@ -3,44 +3,40 @@ import os
 import argparse
 import unittest
 
-from tokenizer.cmds.predict import Predict
+from tokenizer.tokenizer import Tokenizer
 
 class TestTokenizer(unittest.TestCase):
 
     MODEL_DIR = '/tmp/stanza_models'
     
     def setUp(self):
-        self.parser = argparse.ArgumentParser(
-            description='Test argument parser'
-        )
-        self.parser.add_argument('--lang')
-        self.parser.add_argument('--dir')
-        self.parser.add_argument('--verbose', default=True)
-        self.parser.add_argument('--device', default=1)
+        self.args = {
+            'lang': 'it',
+            'dir': self.MODEL_DIR,
+            'verbose': True,
+            'use_gpu': True
+        }
 
     def tearDown(self):
         shutil.rmtree(self.MODEL_DIR)
         
     def test_download_resources(self):
         self.assertTrue(not os.path.exists(self.MODEL_DIR))
-        
-        args = self.parser.parse_args(['--lang', 'it', '--dir', self.MODEL_DIR])
-        tokenizer = Predict()
-        tokenizer(args)
-        
-        self.assertTrue(os.path.exists(args.dir) and not os.path.isfile(args.dir))
-        self.assertTrue(os.path.exists(os.path.join(args.dir, args.lang)))
-        self.assertTrue(os.path.exists(os.path.join(args.dir, args.lang, 'tokenize')))
-
-        sentences = tokenizer.predict('Domani vorrei andare al mare.Speriamo faccia bel tempo.')
-
+        tokenizer = Tokenizer(**self.args)
+        self.assertTrue(os.path.exists(self.args['dir']) and not os.path.isfile(self.args['dir']))
+        self.assertTrue(os.path.exists(os.path.join(self.args['dir'], self.args['lang'])))
+        self.assertTrue(os.path.exists(os.path.join(self.args['dir'], self.args['lang'], 'tokenize')))
     
     def test_tokenize(self):
-        args = self.parser.parse_args(['--lang', 'it', '--dir', '/tmp/stanza_models'])
-        tokenizer = Predict()
-        tokenizer(args)
+        tokenizer = Tokenizer(**self.args)
+        sentences = tokenizer.predict('Domani vorrei andare al mare.Speriamo faccia bel tempo.')
+        self.assertEqual(len(sentences), 2)
 
-        doc = tokenizer.predict('Domani vorrei andare al mare.Speriamo faccia bel tempo.')
-
-        self.assertEqual(len(doc.sentences), 2)
+    def test_corpus_load(self):
+        tokenizer = Tokenizer(**self.args)
+        raw_text_file = '/project/piqasso/Collection/IWPT20/train-dev/UD_Italian-ISDT/it_isdt-ud-dev.txt'
         
+        with open(raw_text_file) as fin:
+            for line in tokenizer.format(tokenizer.predict(fin.read())):
+                if line and not line.startswith('#'):
+                    assert len(line.split('\t')) == 2, line
