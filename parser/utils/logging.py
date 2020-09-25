687b6511cfd716829870c1cc522d00b892ef3b94
Giuseppe Attardi <giuseppe.attardi@unipi.it>
2020-09-25 17:06:48 +0200

    Added logging.

---
+++ parser/utils/logging.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import logging
+import os
+
+
+def get_logger(name):
+    return logging.getLogger(name)
+
+
+def init_logger(logger,
+                path=None,
+                mode='w',
+                level=None,
+                handlers=None,
+                verbose=True):
+    level = level or logging.WARNING
+    if not handlers:
+        handlers = [logging.StreamHandler()]
+        if path:
+            os.makedirs(os.path.dirname(path), exist_ok=True)
+            handlers.append(logging.FileHandler(path, mode))
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        level=level,
+                        handlers=handlers)
+    logger.setLevel(logging.INFO if verbose else logging.WARNING)
+
+
+def progress_bar(iterator,
+                 ncols=None,
+                 bar_format='{l_bar}{bar:36}| {n_fmt}/{total_fmt} {elapsed}<{remaining}, {rate_fmt}{postfix}',
+                 leave=True):
+    return tqdm(iterator,
+                ncols=ncols,
+                bar_format=bar_format,
+                ascii=True,
+                disable=(not is_master()),
+                leave=leave)
+
+
+logger = get_logger('supar')
